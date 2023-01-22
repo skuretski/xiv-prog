8b2b87052e10f62417c3d20d842573dbb9343242
NiRuaG <ajgaurin@gmail.com>
2023-01-22 01:00:40 -0600

    init typescript

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+require("dotenv").config();
+import { Client, Events, GatewayIntentBits } from "discord.js";
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.once(Events.ClientReady, (c) => {
+  console.log(`Ready. Logged in as ${c.user.tag}`);
+});
+
+client.login(process.env["DISCORD_TOKEN"]);
